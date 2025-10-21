a25a08e1181559c9f5e56627d215ecd4632e06c8
DSD-resilience <infantry97@gmail.com>
2025-10-21 15:49:21 -0400

    new file

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,3 +1,10 @@
-# lets practice with strings and indexing first
+# first lets practice with strings and indexing 
 # remember that indexing starts at zero
 print("Hello"[4])
+print("Hello"[-3])
+print("Hello"[0:2])
+
+# check your data types since certain functions require certain data types
+print(type("Hola"))
+print(type(2.24))
+print(type(False))
