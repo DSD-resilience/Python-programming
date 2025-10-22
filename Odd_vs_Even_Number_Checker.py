2c4668271e0a5fe45025c35b2485c10d55e6ccb0
DSD-resilience <infantry97@gmail.com>
2025-10-22 13:09:01 -0400

    new app

---
+++ Odd_vs_Even_Number_Checker.py
@@ -0,0 +1,7 @@
+print("Welcome to the Odd / Even Checker!")
+number = int(input("What is your number? "))
+
+if number % 2 == 0 :
+  print("You have an even number!")
+else:
+  print("You have an odd number!")
--- roller-coaster_check-in_app.py
+++ roller-coaster_check-in_app.py
@@ -1,7 +1,11 @@
-# If - Else Drills
-# spaces are extremely important in Python
+# Roller Coaster Check-in
 
+# screen the guests for the minimum height requirement
 print("Welcome to Roller Coaster Check-in!")
 height = int(input("What is your height in centimeters? "))
 
-If 
+if height >= 120 :
+  print("You can ride the roller coaster!")
+else:
+  print("Sorry, must be taller to ride the roller coaster!")
+
