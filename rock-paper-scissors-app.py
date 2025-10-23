dd4f28fe7e516ee74e46bc80d706fd194800b61c
DSD-resilience <infantry97@gmail.com>
2025-10-23 18:03:30 -0400

    coin flip code

--- rock-paper-scissors-app.py
+++ rock-paper-scissors-app.py
@@ -1,5 +1,16 @@
 import random
 # random number generator
 
-random_integer = random.randint(4,13)
-print(random_integer)
+# random_integer = random.randint(4,13)
+# print(random_integer)
+
+# another_random_number = random.random()
+# print(another_random_number)
+
+# A coin flip would look like this:
+#coin_flip = random.binomialvariate(2,0.5)
+#if coin_flip == 1 :
+    #print("The coin is heads.")
+#else :
+    #print("The coin is tails.")
+
